849a7b7bdbebc381c38cffb54e11eaef2463cff6
Brett Derrington <brett.derrington@gmail.com>
2020-06-02 10:11:15 -0500

    Added javascript files

---
+++ scripts/fish/fishData.js
@@ -0,0 +1,64 @@
+const fishCollection = [
+    {
+        species: "Trigger Fish",
+        length: 12,
+        name: "Bubbles",
+        location: "Bay of Bengal",
+        food: "Baby Squid",
+        picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
+    },
+    {
+        species: "Parrot Fish",
+        length: 2,
+        name: "Angus",
+        location: "Sea of Google",
+        food: "Popcorn",
+        picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
+    },
+    {
+        species: "Trigger Fish",
+        length: 12,
+        name: "Bubbles",
+        location: "Bay of Bengal",
+        food: "Baby Squid",
+        picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
+    },
+    {
+        species: "Parrot Fish",
+        length: 2,
+        name: "Angus",
+        location: "Sea of Google",
+        food: "Popcorn",
+        picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
+    },{
+        species: "Trigger Fish",
+        length: 12,
+        name: "Bubbles",
+        location: "Bay of Bengal",
+        food: "Baby Squid",
+        picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
+    },
+    {
+        species: "Parrot Fish",
+        length: 2,
+        name: "Angus",
+        location: "Sea of Google",
+        food: "Popcorn",
+        picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
+    },{
+        species: "Trigger Fish",
+        length: 12,
+        name: "Bubbles",
+        location: "Bay of Bengal",
+        food: "Baby Squid",
+        picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
+    },
+    {
+        species: "Parrot Fish",
+        length: 2,
+        name: "Angus",
+        location: "Sea of Google",
+        food: "Popcorn",
+        picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
+    }
+]
